5cf452915cf4ce086a95abc7278808ed6f6bc9ac
Bela Barsuhn <belab11@icloud.com>
2024-08-14 18:57:42 +0200

    Erste Programmversion Laterinvokabeln

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿Console.Write("Dateiname: ");
+
+var dateiName = Console.ReadLine();
+var verzeichnisName = "Vokabeln";
+var pfad = Path.Combine(verzeichnisName, dateiName);
+
+if (!File.Exists(pfad))
+{
+    Console.WriteLine($"Die Datei {pfad} gibt es scheinbar nicht!");
+    Environment.Exit(-1);
+}
+
+using var datenStrom = new StreamReader(pfad);
+
+var latein = datenStrom.ReadLine();
+
+while (!string.IsNullOrEmpty(latein))
+{
+    Console.WriteLine($"Übersetze: {latein.Substring(2)}");
+    Console.ReadLine();
+
+    var deutsch = datenStrom.ReadLine();
+
+    Console.WriteLine($"Richtig: {deutsch.Substring(2)}");
+    Console.ReadLine();
+
+    latein = datenStrom.ReadLine();
+}
+
